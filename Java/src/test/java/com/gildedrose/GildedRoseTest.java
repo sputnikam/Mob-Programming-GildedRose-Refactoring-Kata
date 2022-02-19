b6ceee710c286d1119434a5459e908f7794b49a5
sputnika <sputnika@greatevents.net>
2022-02-18 18:37:18 -0600

    Add test case to test quality drop after sell in date

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -14,4 +14,20 @@ class GildedRoseTest {
         assertEquals("fixme", app.items[0].name);
     }
 
+    /**
+     * Once the sell by date has passed, Quality degrades twice as fast
+     */
+    @Test
+    void testQualityDegradation() {
+        int qualitySellIn1d = 40;
+        Item[] items = new Item[] { new Item("foo", 1, qualitySellIn1d) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        int qualitySellIn0d = app.items[0].quality;
+        int dropBeforeSellIn = qualitySellIn1d - qualitySellIn0d;
+        app.updateQuality();
+        int qualitySellInPast1d = app.items[0].quality;
+        int dropAfterSellIn = qualitySellIn0d - qualitySellInPast1d;
+        assertEquals(dropAfterSellIn, dropBeforeSellIn * 2, "Quality should degrade twice as fast after sell by date");
+    }
 }
